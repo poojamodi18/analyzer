3ca86ef03ea4d30d8cc38f94e501cf4c3b1efac7
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-04-15 16:25:37 +0530

    fix: annotation

--- src/main/java/com/polyrepo/analyzer/config/TokenFilter.java
+++ src/main/java/com/polyrepo/analyzer/config/TokenFilter.java
@@ -2,6 +2,7 @@ package com.polyrepo.analyzer.config;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class TokenFilter extends OncePerRequestFilter {
     private final TokenStore tokenStore;
 
