86cac20ce3b09d6233bb213028911c665324f53a
Perin18 <pooja18modi@gmail.com>
2022-04-16 19:52:38 +0530

    add: build

--- src/main/java/com/polyrepo/analyzer/AnalyzerApplication.java
+++ src/main/java/com/polyrepo/analyzer/AnalyzerApplication.java
@@ -2,8 +2,12 @@ package com.polyrepo.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableFeignClients
 public class AnalyzerApplication {
 
 	public static void main(String[] args) {
