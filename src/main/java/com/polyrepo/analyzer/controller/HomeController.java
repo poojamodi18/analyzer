d30dde04c077404ba4fa2ee71f54625ea8fe1704
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-04-25 18:19:03 +0530

    add: dashboard trend

--- src/main/java/com/polyrepo/analyzer/controller/HomeController.java
+++ src/main/java/com/polyrepo/analyzer/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.polyrepo.analyzer.model.UserHistory;
 import com.polyrepo.analyzer.service.UserHistoryService;
 import com.polyrepo.analyzer.service.UserHomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -16,15 +17,29 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @RestController
 @RequestMapping( "/v1/home" )
 public class HomeController {
 
     private final TokenStore tokenStore;
+
+    @Value("${getCommonTrendQuery}")
+    private String getCommonTrendQuery;
+
+    @Value("${getCommonTrend}")
+    private String getCommonTrend;
+
+    @Value("${getTodayCommonTrend}")
+    private String getTodayCommonTrend;
+
+    @Value("${graphQLAccessPrefix}")
+    private String graphQLAccessPrefix;
 
     @Autowired
     private UserHomeService userHomeService;
@@ -50,5 +65,49 @@ public class HomeController {
     @GetMapping("/recentHistory")
     public ResponseEntity<Map<String, UserHistory>> getRecentHistory(@RequestHeader("id") String userId){
         return new ResponseEntity<>(userHistoryService.getUserHistory(Integer.parseInt(userId)), HttpStatus.OK);
+    }
+
+    @GetMapping("/commonTrend")
+    public ResponseEntity<Map<String,Object>> getCommonTrend(){
+        Date date = new Date();
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int month = localDate.getMonthValue();
+
+        DateTimeFormatter monthYearFormatter = DateTimeFormatter.ofPattern("MMM");
+        DateTimeFormatter numberMonthYearFormatter = DateTimeFormatter.ofPattern("MM");
+        DateTimeFormatter yearFormatter = DateTimeFormatter.ofPattern("yyyy");
+
+        YearMonth thisMonth    = YearMonth.now();
+
+        List<String> queryday = new ArrayList<>();
+
+        System.out.println("\n\n\nLOOP");
+
+        for (int i = 0; i <month; i++) {
+            YearMonth lastMonth    = thisMonth.minusMonths(i);
+            queryday.add(lastMonth.format(monthYearFormatter));
+            queryday.add(lastMonth.format(yearFormatter)+"-"+ lastMonth.format(numberMonthYearFormatter)+"-01");
+        }
+
+        System.out.println(queryday);
+
+
+        int index = 0;
+
+        String query = String.format(getCommonTrend, queryday.get(0), queryday.get(1), queryday.get(0), queryday.get(1),
+                queryday.get(0), queryday.get(1), queryday.get(0), queryday.get(1), queryday.get(0), queryday.get(1));
+
+        String finalQuery = "";
+        for (int i = 0; i <month; i++) {
+                // 0 -> 0 1
+                // 1 -> 2 3
+                // 2 -> 4 5
+                // 3 -> 6 7
+        }
+
+
+        System.out.println(query);
+
+        return null;
     }
 }
