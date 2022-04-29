974ff6a03b38ca5d5fc53c546d982b55d41f02f4
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-04-29 15:35:10 +0530

    add: url

--- src/main/java/com/polyrepo/analyzer/AnalyzerApplication.java
+++ src/main/java/com/polyrepo/analyzer/AnalyzerApplication.java
@@ -4,16 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableFeignClients
 @EnableAutoConfiguration
-public class AnalyzerApplication {
+public class AnalyzerApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyzerApplication.class, args);
 	}
 
+//	@Override
+//	public void configurePathMatch(PathMatchConfigurer configurer) {
+//		UrlPathHelper urlPathHelper = new UrlPathHelper();
+//		urlPathHelper.setUrlDecode(false);
+//		configurer.setUrlPathHelper(urlPathHelper);
+//	}
 }
