4a8c057b5112cc013324ee91502fcce29f5fbddb
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-04-29 16:09:54 +0530

    add: firebase setup

--- src/main/java/com/polyrepo/analyzer/AnalyzerApplication.java
+++ src/main/java/com/polyrepo/analyzer/AnalyzerApplication.java
@@ -1,10 +1,18 @@
 package com.polyrepo.analyzer;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -12,7 +20,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration
 public class AnalyzerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+
+		ClassLoader classLoader = AnalyzerApplication.class.getClassLoader();
+
+		File file = new File(Objects.requireNonNull(classLoader.getResource("ServiceAccountKey.json")).getFile());
+		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		FirebaseApp.initializeApp(options);
+
+
 		SpringApplication.run(AnalyzerApplication.class, args);
 	}
 
