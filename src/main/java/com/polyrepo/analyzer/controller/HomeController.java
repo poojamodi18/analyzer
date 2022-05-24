cf9ec6b4cd7c0abfa3dcaf720b2429b0ee1f5eff
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-05-24 09:58:47 +0530

    fix: sonar suggestion

--- src/main/java/com/polyrepo/analyzer/controller/HomeController.java
+++ src/main/java/com/polyrepo/analyzer/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.polyrepo.analyzer.controller;
 
-import com.polyrepo.analyzer.config.TokenStore;
 import com.polyrepo.analyzer.constant.StringConstants;
 import com.polyrepo.analyzer.model.User;
 import com.polyrepo.analyzer.model.UserHistory;
@@ -11,28 +10,20 @@ import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @RestController
 @RequestMapping( "/v1/home" )
 public class HomeController {
 
-    private final TokenStore tokenStore;
 
     @Autowired
     private UserHomeService userHomeService;
@@ -42,9 +33,6 @@ public class HomeController {
 
     private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    public HomeController( TokenStore tokenStore ) {
-        this.tokenStore = tokenStore;
-    }
 
     @GetMapping
     public ResponseEntity<User> getUserName(@AuthenticationPrincipal( expression = "attributes['name']" ) String username
@@ -54,7 +42,7 @@ public class HomeController {
             , @AuthenticationPrincipal( expression = "attributes['html_url']") String url
                                            ) {
 
-        return new ResponseEntity<User>(userHomeService.getUserDetails(username,name,login,avatarUrl,url), HttpStatus.OK);
+        return new ResponseEntity<>(userHomeService.getUserDetails(username,name,login,avatarUrl,url), HttpStatus.OK);
     }
 
     @GetMapping("/recentHistory")
